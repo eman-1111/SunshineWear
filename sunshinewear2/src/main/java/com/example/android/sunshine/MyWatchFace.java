5f80cdd18358c174683bdd1c898bcdb398181fe5
Eman <eman.ashour1111@gmail.co>
2017-03-23 13:57:15 +0200

    Change packages name

--- sunshinewear2/src/main/java/com/example/android/sunshine/MyWatchFace.java
+++ sunshinewear2/src/main/java/com/example/android/sunshine/MyWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.link.ideas.sunshinewear;
+package com.example.android.sunshine;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
--- sunshinewear2/src/main/java/com/example/android/sunshine/SunshineWearUtil.java
+++ sunshinewear2/src/main/java/com/example/android/sunshine/SunshineWearUtil.java
@@ -1,4 +1,4 @@
-package com.link.ideas.sunshinewear;
+package com.example.android.sunshine;
 
 import android.util.Log;
 
